cb6ee7e6b51671c16e3fb716210d43de6ee95189
vikas <vikaskmr2411@gmail.com>
2025-11-25 00:10:49 +0530

    added some code

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ from langchain_anthropic import ChatAnthropic
 
 # Load environment variables from .env file
 load_dotenv()  
+
+llm = ChatOpenAI(model="gpt-4o-mini")
+llm2 = ChatAnthropic(model="claude-3-5-sonnet-20241022")
+#add gemini also as i have premium
+
+response = llm.invoke("What is the meaning of life?")
+
+print("Response from OpenAI:", response)
